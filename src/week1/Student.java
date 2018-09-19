364fa5befe83a00651d88f7c57e3720da2de848a
minhanh <minhanh.th99@gmail.com>
2018-09-19 17:28:05 +0700

    lan 2

--- src/week1/Student.java
+++ src/week1/Student.java
@@ -10,43 +10,55 @@ public class Student {
         id="000";
         group="INT2208";
     }
+
     public Student(String name_, String id_, String group_, String email_){
         name= name_;
         id=id_;
         group=group_;
         email=email_;
     }
+
     public Student(Student s){
         name=s.getname();
         id=s.getid();
         email=s.getemail();
         group=s.getgroup();
     }
+
     public String getname(){
         return name;
     }
+
     public void setname(String n){
         name = n;
     }
+
     public String getgroup(){
         return group;
     }
+
     public void setgroup(String g){
         group = g;
     }
+
     public String getid(){
         return id;
     }
+
     public void setid(String i){
         id = i;
     }
+
     public String getemail(){
         return email;
     }
+
     public void setemail(String e){
         email = e;
     }
+
     public void getInfo(){
         System.out.println("Name SV: " +name +"\nID: "+ id+"\nGroup: " + group+"\nEmail: "+email);
     }
+
 }
