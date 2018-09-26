2547020a7032f66845d1ba351bb09afc450a9032
dominan <43226461+dominan@users.noreply.github.com>
2018-09-26 19:27:41 +0700

    Update Week3Test.java

--- test/week3/Week3Test.java
+++ test/week3/Week3Test.java
@@ -7,6 +7,7 @@ public class Week3Test {
 
 
     // TODO: Viết 5 testcase cho phương thức max()
+    @Test
     public void maxtest(){
 
         Assert.assertEquals(5, Week3.max(3,5));
@@ -16,6 +17,7 @@ public class Week3Test {
         Assert.assertEquals(4596, Week3.max(3,4596));
 
     }
+    @Test
     public void minOfArraytest(){
         int a1[]={2,8,6,4,3,7,1},a2[]={8895,547,2216,249,153},a3[]={8925,451,662154,112};
         int a4[]={125,148,364,458,657,6242,652,12,48}, a5[]={565,124,3365,541,0,99,1862,6452,33668,11544,122562,15};
@@ -25,6 +27,7 @@ public class Week3Test {
         Assert.assertEquals(12,Week3.minOfArray(a4));
         Assert.assertEquals(0,Week3.minOfArray(a5));
     }
+    @Test
     public void calculateBMItest(){
         Assert.assertEquals("Thiếu cân", Week3.calculateBMI(36.5,1.47));
         Assert.assertEquals("Bình thường", Week3.calculateBMI(56.6,1.7));
