03ee1733ca9e08018897b40ea071c8caaf15cb17
dominan <43226461+dominan@users.noreply.github.com>
2018-09-27 09:48:51 +0700

    Update Task1.java

--- src/week2/task1/Task1.java
+++ src/week2/task1/Task1.java
@@ -17,7 +17,7 @@ public class Task1 {
 	
         int [] arr= new int [n];
         arr[0]=0; arr[1]=1;
-	for(int i=2; i<n; i++){
+	for(int i=2; i<=n; i++){
 		arr[i]=arr[i-1]+arr[i-2];
 	}
         int m=arr[n];
