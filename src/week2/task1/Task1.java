ce0728dd0fc4ea502f391ef03d7f27f083a11a5e
dominan <43226461+dominan@users.noreply.github.com>
2018-09-27 09:59:20 +0700

    Update Task1.java

--- src/week2/task1/Task1.java
+++ src/week2/task1/Task1.java
@@ -17,12 +17,8 @@ public class Task1 {
     public static int fibonacci(int n) {
         // TODO: Tìm số fibonacci ở vị trí n
 	
-        int [] arr= new int [n];
-        arr[0]=0; arr[1]=1;
-	for(int i=2; i<=n; i++){
-		arr[i]=arr[i-1]+arr[i-2];
-	}
-        int m=arr[n];
-        return m;
+        if(n==0) return 0;
+	else if(n==1) return 1;
+        else return fibonacci(n-1)+fibonacci(n-2);
     }
 }
