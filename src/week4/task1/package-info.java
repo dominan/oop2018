690633e296a1de48e65790943de92f1d4d072e19
dominan <minhanh.th99@gmail.com>
2018-10-07 20:09:00 +0700

    Update src/week4/task1/package-info.java

--- src/week4/task1/package-info.java
+++ src/week4/task1/package-info.java
@@ -1,4 +1,4 @@
-/**
+﻿/**
  * Package bài làm cho Câu 1
  * TODO: Khai báo các lớp (class) theo Câu 1 yêu cầu. 1 file chỉ khai báo 1 class
  * TODO: Mô tả các quan hệ giữa các class và thêm thuộc tính, phương thức cho chúng (mỗi class ít nhất 2 phương thức, 2 thuộc tính)
@@ -6,4 +6,4 @@
  * @author cuong
  * @version 0.1
  */
-package week4.task1;
+import java.util.Scanner;
