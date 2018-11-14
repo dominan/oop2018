78d045eb8c627922ed98bc667c3daac9393562ca
dominan <43226461+dominan@users.noreply.github.com>
2018-11-15 01:25:16 +0700

    Update Task2.java

--- src/week10/Task2.java
+++ src/week10/Task2.java
@@ -1,4 +1,26 @@
 package week10;
 
-public class Task2 {
+import java.util.Random;
+public class Task2 {	
+	public static void BubleSort(int a[]) {
+            int temp;
+            for(int i = 0; i < 999; i ++) {
+		for(int j = 0; j < 999 - i; j ++) {
+                    if(a[j] > a[j+1]) {
+			temp = a[j];
+			a[j] = a[j+1];
+			a[j+1] = temp;					
+                    }			
+		}
+            }
+	}
+	public static void main(String[] args) {
+            int []a = new int[1000];
+            Random random = new Random(); 
+            for(int i = 0; i < 1000 ; i ++){
+		a[i] = random.nextInt(1000);			
+            }		
+            BubleSort(a);
+            for(int i=0; i<1000; i++)System.out.print(a[i]+" ");
+	}
 }
