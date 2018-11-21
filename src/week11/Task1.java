d416c678d6bc53d220ca291e5af0488750c36b1f
dominan <43226461+dominan@users.noreply.github.com>
2018-11-21 21:58:11 +0700

    Update Task1.java

--- src/week11/Task1.java
+++ src/week11/Task1.java
@@ -1,4 +1,33 @@
 package week11;
 
 public class Task1 {
+  public static < E extends Comparable<E>> void printArray( E[] inputArray )
+   {
+       // Hien thi cac phan tu mang
+       
+         for(int i=0; i<inputArray.length-1; i++){
+             if(inputArray[i].compareTo(inputArray[i+1])>0){
+                 E name = inputArray[i];
+                 inputArray[i]=inputArray[i+1];
+                 inputArray[i+1]=name;
+             }
+         }
+         for (E element : inputArray){
+             System.out.printf(" %s ", element);
+         }
+    }
+    public static void main( String args[] )
+    {
+        // Tao cac mang Integer, Double va Character
+        Integer[] intArray = { 1, 7, 3, 4, 5 };      
+        Character[] charArray = { 'H', 'E', 'L', 'L', 'O' };
+
+        System.out.println( "Mang intArray bao gom:" );
+        printArray( intArray  ); // truyen mot mang Integer
+
+        System.out.println( "\nMang charArray bao gom:" );
+        printArray( charArray ); // truyen mot mang Character
+      
+
+    } 
 }
